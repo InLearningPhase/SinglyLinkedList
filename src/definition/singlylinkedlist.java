44cc4473ac666481dea39a93ce256fdb4c9108f4
Tanesh Gupta <40913077+InLearningPhase@users.noreply.github.com>
2020-03-02 01:54:37 +0530

    Add node class

--- src/definition/singlylinkedlist.java
+++ src/definition/singlylinkedlist.java
@@ -1,4 +1,59 @@
 package definition;
 
-public class singlylinkedlist {
+
+import adt.linkedlistADT;
+
+public class singlylinkedlist<E> implements linkedlistADT<E> {
+
+
+
+
+    @Override
+    public void add(E item) {
+
+    }
+
+    @Override
+    public E remove() {
+        return null;
+    }
+
+    @Override
+    public int search(E item) {
+        return 0;
+    }
+
+    @Override
+    public void sort() {
+
+    }
+
+    @Override
+    public void print() {
+
+    }
+
+    private static class Node<E> {
+
+        private E data;
+        private Node<E> next;
+
+        private Node(E data) {
+            this.data = data;
+        }
+
+        private Node(E data, Node<E> next){
+            this.data = data;
+            this.next = next;
+        }
+
+        public E getData() {
+            return data;
+        }
+
+        public Node<E> getNext() {
+            return next;
+        }
+    }
+
 }
